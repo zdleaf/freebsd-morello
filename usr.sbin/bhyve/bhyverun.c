0e297a8d1cdbf783466e6a8c51a284c3535c187d
Andrew Turner <at718@cam.ac.uk>
2020-04-30 13:48:19 +0000

    Attach the GIC

--- usr.sbin/bhyve/bhyverun.c
+++ usr.sbin/bhyve/bhyverun.c
@@ -1249,6 +1249,8 @@ main(int argc, char *argv[])
 	error = bootrom_init(ctx, "/root/u-boot.bin");
 	assert(error == 0);
 	init_uart();
+	vm_attach_vgic(ctx, 0x2f000000UL, 0x10000UL, 0x2f100000UL, 0x200000UL,
+	    0x2f300000UL, 0x10000UL);
 #ifdef __amd64__
 	init_inout();
 	atkbdc_init(ctx);
