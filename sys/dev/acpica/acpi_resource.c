4d12932945950ef7dcdb04d6f47d2c9fb3d2f8ef
Ruslan Bukin <br@bsdpad.com>
2020-06-10 15:15:32 +0100

    List all the coresight devices.

--- sys/dev/acpica/acpi_resource.c
+++ sys/dev/acpica/acpi_resource.c
@@ -484,8 +484,17 @@ acpi_parse_resources(device_t dev, ACPI_HANDLE handle,
     if (acpi_MatchHid(handle, "ARMH0011") != ACPI_MATCHHID_NOMATCH)
 	    arc.ignore_producer_flag = true;
 
-    /* ARM Coresight on N1SDP set ResourceProducer on memory resources. */
-    if (acpi_MatchHid(handle, "ARMHC500") != ACPI_MATCHHID_NOMATCH)
+    /*
+     * ARM Coresight on N1SDP set ResourceProducer on memory resources.
+     * Coresight devices: ETM, STM, TPIU, ETF/ETR, REP, SFUN, FUN.
+     */
+    if (acpi_MatchHid(handle, "ARMHC500") != ACPI_MATCHHID_NOMATCH ||
+        acpi_MatchHid(handle, "ARMHC502") != ACPI_MATCHHID_NOMATCH ||
+        acpi_MatchHid(handle, "ARMHC979") != ACPI_MATCHHID_NOMATCH ||
+        acpi_MatchHid(handle, "ARMHC97C") != ACPI_MATCHHID_NOMATCH ||
+        acpi_MatchHid(handle, "ARMHC98D") != ACPI_MATCHHID_NOMATCH ||
+        acpi_MatchHid(handle, "ARMHC9FE") != ACPI_MATCHHID_NOMATCH ||
+        acpi_MatchHid(handle, "ARMHC9FF") != ACPI_MATCHHID_NOMATCH)
 	    arc.ignore_producer_flag = true;
 
     status = AcpiWalkResources(handle, "_CRS", acpi_parse_resource, &arc);
