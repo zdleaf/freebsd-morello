d912c2518d7645606c2a1b06bcdd10d8171823d3
Ruslan Bukin <br@bsdpad.com>
2020-10-20 10:49:35 +0100

    Cleanup

--- sys/arm64/include/pmap.h
+++ sys/arm64/include/pmap.h
@@ -188,8 +188,8 @@ bool	pmap_get_tables(pmap_t, vm_offset_t, pd_entry_t **, pd_entry_t **,
 int	pmap_fault(pmap_t, uint64_t, uint64_t);
 
 /* System MMU (SMMU). */
-int pmap_senter(pmap_t pmap, vm_offset_t va, vm_paddr_t pa,
-    vm_prot_t prot, u_int flags);
+int pmap_senter(pmap_t pmap, vm_offset_t va, vm_paddr_t pa, vm_prot_t prot,
+    u_int flags);
 int pmap_sremove(pmap_t pmap, vm_offset_t va);
 int pmap_sremove_all(pmap_t pmap);
 
