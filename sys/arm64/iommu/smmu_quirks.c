3923861398b1a9b972e0a6584acd725a202c349d
Ruslan Bukin <br@bsdpad.com>
2020-11-03 11:27:17 +0000

    Change copyright for a file that was developed during October

--- sys/arm64/iommu/smmu_quirks.c
+++ sys/arm64/iommu/smmu_quirks.c
@@ -3,10 +3,8 @@
  *
  * Copyright (c) 2020 Ruslan Bukin <br@bsdpad.com>
  *
- * This software was developed by SRI International and the University of
- * Cambridge Computer Laboratory (Department of Computer Science and
- * Technology) under DARPA contract HR0011-18-C-0016 ("ECATS"), as part of the
- * DARPA SSITH research programme.
+ * This work was supported by Innovate UK project 105694, "Digital Security
+ * by Design (DSbD) Technology Platform Prototype".
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
