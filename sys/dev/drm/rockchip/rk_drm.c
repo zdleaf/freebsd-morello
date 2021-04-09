510a929dbe1e6f015d82561f5289c33bed4d1dff
Ruslan Bukin <br@bsdpad.com>
2021-04-09 16:23:07 +0100

    implement rockchip_drm_gem and import_sg_table. it seems needed after mesa3d update. Not sure why

--- sys/dev/drm/rockchip/rk_drm.c
+++ sys/dev/drm/rockchip/rk_drm.c
@@ -71,6 +71,11 @@ struct rk_drm_softc {
 	struct drm_fb_cma	*fb;
 };
 
+struct rockchip_gem_object {
+	struct drm_gem_object	base;   /* Must go first */
+	struct sg_table		*sgt;
+};
+
 static struct ofw_compat_data compat_data[] = {
 	{ "rockchip,display-subsystem",	1 },
 	{ NULL,				0 }
@@ -79,6 +84,9 @@ static struct ofw_compat_data compat_data[] = {
 static int rk_drm_probe(device_t dev);
 static int rk_drm_attach(device_t dev);
 static int rk_drm_detach(device_t dev);
+
+MALLOC_DECLARE(M_RKDRM);
+MALLOC_DEFINE(M_RKDRM, "rockchip", "Rockchip DRM");
 
 struct sg_table *
 rockchip_gem_prime_get_sg_table(struct drm_gem_object *obj)
@@ -96,14 +104,128 @@ rockchip_gem_prime_get_sg_table(struct drm_gem_object *obj)
 	return (sgt);
 }
 
-struct drm_gem_object *
-rockchip_gem_prime_import_sg_table(struct drm_device *drm,
-    struct dma_buf_attachment *attach, struct sg_table *sg)
+static void
+rockchip_gem_free_object(struct drm_gem_object *obj)
+{
+	struct rockchip_gem_object *bo;
+
+	bo = (struct rockchip_gem_object *)obj;
+
+	if (obj->import_attach)
+		drm_prime_gem_destroy(obj, bo->sgt);
+
+	drm_gem_object_release(obj);
+
+	free(bo, M_RKDRM);
+}
+
+int
+rockchip_gem_open(struct drm_gem_object *obj, struct drm_file *file_priv)
 {
 
-	printf("%s: implement me\n", __func__);
-	panic("%s\n", __func__);
+	return (0);
+}
+
+void
+rockchip_gem_close(struct drm_gem_object *obj, struct drm_file *file_priv)
+{
+
+}
+
+void
+rockchip_gem_print_info(struct drm_printer *p, unsigned int indent,
+    const struct drm_gem_object *obj)
+{
+
+	printf("%s\n", __func__);
+}
+
+static int
+rockchip_gem_pin(struct drm_gem_object *obj)
+{
+
+	printf("%s\n", __func__);
+
+	return (0);
+}
+
+void
+rockchip_gem_unpin(struct drm_gem_object *obj)
+{
+
+	printf("%s\n", __func__);
+}
+
+struct sg_table *
+rockchip_gem_get_sg_table(struct drm_gem_object *obj)
+{
+
+	printf("%s\n", __func__);
+
 	return (NULL);
+}
+
+void *
+rockchip_gem_vmap(struct drm_gem_object *obj)
+{
+
+	printf("%s\n", __func__);
+
+	return (0);
+}
+
+void
+rockchip_gem_vunmap(struct drm_gem_object *obj, void *vaddr)
+{
+
+	printf("%s\n", __func__);
+}
+
+int
+rockchip_gem_mmap(struct drm_gem_object *obj, struct vm_area_struct *vma)
+{
+
+	printf("%s\n", __func__);
+
+	return (0);
+}
+
+static const struct drm_gem_object_funcs rockchip_gem_funcs = {
+	.free = rockchip_gem_free_object,
+	.open = rockchip_gem_open,
+	.close = rockchip_gem_close,
+	.print_info = rockchip_gem_print_info,
+	.pin = rockchip_gem_pin,
+	.unpin = rockchip_gem_unpin,
+	.get_sg_table = rockchip_gem_get_sg_table,
+	.vmap = rockchip_gem_vmap,
+	.vunmap = rockchip_gem_vunmap,
+	.mmap = rockchip_gem_mmap,
+};
+
+struct drm_gem_object *
+rockchip_gem_prime_import_sg_table(struct drm_device *dev,
+    struct dma_buf_attachment *attach, struct sg_table *sg)
+{
+	struct rockchip_gem_object *bo;
+	size_t size;
+	int error;
+
+	bo = malloc(sizeof(*bo), M_RKDRM, M_ZERO | M_WAITOK);
+	bo->base.funcs = &rockchip_gem_funcs;
+	bo->sgt = sg;
+
+	size = PAGE_ALIGN(attach->dmabuf->size);
+
+	drm_gem_object_init(dev, &bo->base, size);
+
+	error = drm_gem_create_mmap_offset(&bo->base);
+	if (error != 0) {
+		printf("%s: Failed to create mmap offset.\n", __func__);
+		return (NULL);
+	}
+
+	return (&bo->base);
 }
 
 static int
