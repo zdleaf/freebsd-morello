07e58b10f125fd9fd06fcdfa2956735305af1625
Andrew Turner <andrew@FreeBSD.org>
2021-06-23 14:57:25 +0000

    Start moving guest register management to an asm macro

--- sys/arm64/vmm/hyp_macros.h
+++ sys/arm64/vmm/hyp_macros.h
@@ -164,50 +164,6 @@
 
 
 /*
- * We use x0 to load the hypctx address from TPIDR_EL2 and x1 and x2 as
- * temporary registers to compute the hypctx member addresses. To save the guest
- * values at first we push them on the stack, use these temporary registers to
- * save the rest of the registers and at the end we pop the values from the
- * stack and save them.
- */
-#define SAVE_GUEST_X_REGS()				\
-	/* Push x0 */					\
-	str	x0, [sp, #-16]!;			\
-	/* Restore hypctx address */			\
-	mrs	x0, tpidr_el2;				\
-	/* Push x1 and x2 */				\
-	stp	x1, x2, [sp, #-16]!;			\
-							\
-	/* Save the other registers */			\
-	SAVE_REG_PAIR(HYPCTX_REGS, X3, X4);		\
-	SAVE_REG_PAIR(HYPCTX_REGS, X5, X6);		\
-	SAVE_REG_PAIR(HYPCTX_REGS, X7, X8);		\
-	SAVE_REG_PAIR(HYPCTX_REGS, X9, X10);		\
-	SAVE_REG_PAIR(HYPCTX_REGS, X11, X12);		\
-	SAVE_REG_PAIR(HYPCTX_REGS, X13, X14);		\
-	SAVE_REG_PAIR(HYPCTX_REGS, X15, X16);		\
-	SAVE_REG_PAIR(HYPCTX_REGS, X17, X18);		\
-	SAVE_REG_PAIR(HYPCTX_REGS, X19, X20);		\
-	SAVE_REG_PAIR(HYPCTX_REGS, X21, X22);		\
-	SAVE_REG_PAIR(HYPCTX_REGS, X23, X24);		\
-	SAVE_REG_PAIR(HYPCTX_REGS, X25, X26);		\
-	SAVE_REG_PAIR(HYPCTX_REGS, X27, X28);		\
-	SAVE_REG(HYPCTX_REGS, X29);			\
-	SAVE_REG(HYPCTX_REGS, LR);			\
-							\
-	/* Pop and save x1 and x2 */			\
-	ldp	x1, x2, [sp], #16;			\
-	mov	x3, #HYPCTX_REGS_X1;			\
-	add	x3, x0, x3;				\
-	stp	x1, x2, [x3];				\
-	/* Pop and save x0 */				\
-	ldr	x1, [sp], #16;				\
-	mov	x2, #HYPCTX_REGS_X0;			\
-	add	x2, x2, x0;				\
-	str	x1, [x2];
-
-
-/*
  * Save all the guest registers. Start by saving the regular registers first
  * because those will be used as temporary registers for accessing the hypctx
  * member addresses.
@@ -219,8 +175,6 @@
  * x0 - struct hypctx address
  */
 #define	SAVE_GUEST_REGS()				\
-	SAVE_GUEST_X_REGS();				\
-							\
 	SAVE_REG(HYPCTX, FP);				\
 							\
 	SAVE_SYS_REG32(HYPCTX_VTIMER_CPU, CNTKCTL_EL1);	\
@@ -346,41 +300,6 @@
 #define	LOAD_REG(prefix, reg)				\
 	mov	x1, prefix ##_ ##reg;			\
 	ldr	reg, [x0, x1];
-
-
-/*
- * We use x1 as a temporary register to store the hypctx member offset and x0
- * to hold the hypctx address. We load the guest x0 and x1 register values in
- * registers x2 and x3, push x2 and x3 on the stack and then we restore x0 and
- * x1.
- */
-#define	LOAD_GUEST_X_REGS()				\
-	mov	x1, #HYPCTX_REGS_X0;			\
-	/* x1 now holds the address of hypctx reg x0 */	\
-	add	x1, x1, x0;				\
-	/* Make x2 = guest x0 and x3 = guest x1 */	\
-	ldp	x2, x3, [x1];				\
-	stp	x2, x3, [sp, #-16]!;			\
-							\
-	/* Load the other registers */			\
-	LOAD_REG_PAIR(HYPCTX_REGS, X2, X3);		\
-	LOAD_REG_PAIR(HYPCTX_REGS, X4, X5);		\
-	LOAD_REG_PAIR(HYPCTX_REGS, X6, X7);		\
-	LOAD_REG_PAIR(HYPCTX_REGS, X8, X9);		\
-	LOAD_REG_PAIR(HYPCTX_REGS, X10, X11);		\
-	LOAD_REG_PAIR(HYPCTX_REGS, X12, X13);		\
-	LOAD_REG_PAIR(HYPCTX_REGS, X14, X15);		\
-	LOAD_REG_PAIR(HYPCTX_REGS, X16, X17);		\
-	LOAD_REG_PAIR(HYPCTX_REGS, X18, X19);		\
-	LOAD_REG_PAIR(HYPCTX_REGS, X20, X21);		\
-	LOAD_REG_PAIR(HYPCTX_REGS, X22, X23);		\
-	LOAD_REG_PAIR(HYPCTX_REGS, X24, X25);		\
-	LOAD_REG_PAIR(HYPCTX_REGS, X26, X27);		\
-	LOAD_REG_PAIR(HYPCTX_REGS, X28, X29);		\
-	LOAD_REG(HYPCTX_REGS, LR);			\
-							\
-	/* Pop guest x0 and x1 from the stack */	\
-	ldp	x0, x1, [sp], #16;			\
 
 
 #define	LOAD_ARRAY_REG64(reg, src, remaining)		\
@@ -545,8 +464,6 @@
 	add	x1, x1, x0;				\
 	ldr	x2, [x1];				\
 	msr	sp_el1, x2;				\
-							\
-	LOAD_GUEST_X_REGS();				\
 
 
 /*
