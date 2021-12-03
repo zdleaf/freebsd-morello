a658ece92049b02a8064eafc8e5da5cb6b592a9d
Ruslan Bukin <br@FreeBSD.org>
2021-12-03 17:11:37 +0000

    add script

---
+++ virtio_gpu.sh
@@ -0,0 +1,19 @@
+A=CONF
+if [ "$1" = "fast" ]; then
+	A=FAST
+fi
+
+#./tools/tools/makeroot/makeroot.sh -s 32m -f basic.files riscv.img /home/br/world-riscv/
+
+export MK_CTF=no
+make -j8 TARGET=riscv KERN${A}=GENERIC buildkernel || exit 1
+
+#sh ./sys/tools/embed_mfs.sh /usr/obj/usr/home/br/dev/freebsd-head/riscv.riscv64/sys/GENERIC/kernel ./riscv.img || exit 2
+
+# makefs -f 1000000 -s 8g /mnt/riscv-full.img /home/br/world-riscv/
+
+# make -j8 TARGET=riscv -DNO_ROOT DESTDIR=/mnt/world-riscv installworld
+
+scp /usr/obj/usr/home/br/dev/freebsd-head/riscv.riscv64/sys/GENERIC/kernel 10.8.0.1:~/virtgl_kernel
+
+makefs -D -f 1000000 -s 8g /mnt/riscv-full.img METALOG
