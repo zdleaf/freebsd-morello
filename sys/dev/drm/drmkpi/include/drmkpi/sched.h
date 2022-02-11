8ba6d51a532035cc3ac4da9016426b9d9223b1ec
Ruslan Bukin <br@FreeBSD.org>
2022-02-11 13:26:54 +0000

    revert 'current' macro

--- sys/dev/drm/drmkpi/include/drmkpi/sched.h
+++ sys/dev/drm/drmkpi/include/drmkpi/sched.h
@@ -52,7 +52,11 @@ struct drm_task {
 	atomic_t state;
 };
 
-#define	current	(struct drm_task *)curthread
+#define	current	({ \
+	struct thread *__td = curthread; \
+	linux_set_current(__td); \
+	((struct drm_task *)__td->td_lkpi_task); \
+})
 
 bool drmkpi_signal_pending(struct drm_task *task);
 
