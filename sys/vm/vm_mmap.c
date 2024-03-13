4e126ad645681ab66c7119c74dfa5dbabb70d0ea
Ruslan Bukin <br@freebsd.org>
2024-03-13 17:06:53 +0000

    fix build

--- sys/vm/vm_mmap.c
+++ sys/vm/vm_mmap.c
@@ -599,7 +599,6 @@ kern_munmap(struct thread *td, uintptr_t addr0, size_t size)
 	struct hwt_record_entry ent;
 	if (rv == KERN_SUCCESS) {
 		ent.addr = (uintptr_t) addr;
-		ent.size = (size_t) size;
 		ent.record_type = HWT_RECORD_MUNMAP;
 		HWT_CALL_HOOK(td, HWT_RECORD, &ent);
 	}
