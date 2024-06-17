f4c5ab84aa980f4cff23858b4d0c4a88af70422a
Ruslan Bukin <br@bsdpad.com>
2024-06-17 14:04:31 +0100

    fix my build script

--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@ if [ "$1" = "fast" ]; then
 	A=FAST
 fi
 
-make -j12 TARGET=arm64 KERN${A}=GENERIC buildkernel || exit 1
+make -j12 TARGET=arm64 KERN${A}=GENERIC-MORELLO buildkernel || exit 1
 
-cp /usr/obj/usr/home/br/dev/freebsd/arm64.aarch64/sys/GENERIC/kernel /tftpboot/root/boot/kernel/kernel
+cp /usr/obj/usr/home/br/dev/freebsd/arm64.aarch64/sys/GENERIC-MORELLO/kernel /tftpboot/root/boot/kernel/kernel || exit 2
 
-scp /usr/obj/usr/home/br/dev/freebsd/arm64.aarch64/sys/GENERIC/kernel 10.2.0.156:~/modules/boot/kernel/
+scp /usr/obj/usr/home/br/dev/freebsd/arm64.aarch64/sys/GENERIC-MORELLO/kernel 10.2.0.156:~/modules/boot/kernel/ || exit 3
 
-scp /usr/obj/usr/home/br/dev/freebsd/arm64.aarch64/sys/GENERIC/modules/usr/home/br/dev/freebsd/sys/modules/coresight/*/*ko 10.2.0.156:~/modules/
+scp /usr/obj/usr/home/br/dev/freebsd/arm64.aarch64/sys/GENERIC-MORELLO/modules/usr/home/br/dev/freebsd/sys/modules/coresight/*/*ko 10.2.0.156:~/modules/ || exit 4
 
-scp /usr/obj/usr/home/br/dev/freebsd/arm64.aarch64/sys/GENERIC/modules/usr/home/br/dev/freebsd/sys/modules/hwt/hwt.ko 10.2.0.156:~/modules/
+scp /usr/obj/usr/home/br/dev/freebsd/arm64.aarch64/sys/GENERIC-MORELLO/modules/usr/home/br/dev/freebsd/sys/modules/hwt/hwt.ko 10.2.0.156:~/modules/ || exit 5
