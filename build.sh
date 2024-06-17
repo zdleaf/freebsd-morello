48253b7ea99fc5fa2a7b526fc0a105768b090cf9
Ruslan Bukin <br@bsdpad.com>
2024-06-17 15:32:28 +0100

    copy hwt binary

--- build.sh
+++ build.sh
@@ -12,3 +12,5 @@ scp /usr/obj/usr/home/br/dev/freebsd/arm64.aarch64/sys/GENERIC-MORELLO/kernel 10
 scp /usr/obj/usr/home/br/dev/freebsd/arm64.aarch64/sys/GENERIC-MORELLO/modules/usr/home/br/dev/freebsd/sys/modules/coresight/*/*ko 10.2.0.156:~/modules/ || exit 4
 
 scp /usr/obj/usr/home/br/dev/freebsd/arm64.aarch64/sys/GENERIC-MORELLO/modules/usr/home/br/dev/freebsd/sys/modules/hwt/hwt.ko 10.2.0.156:~/modules/ || exit 5
+
+scp /usr/obj/usr/home/br/dev/freebsd/arm64.aarch64/usr.sbin/hwt/hwt 10.2.0.156:~/
